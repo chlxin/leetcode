8c1f152d9e3e6c63b50943555dc9aa4a4d96a499
linxinc <18842879509@163.com>
2018-07-12 01:25:10 +0800

    104，凌晨刷一题

---
+++ chapter/104.maximum_depth_binary_tree.go
@@ -0,0 +1,26 @@
+package chapter
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
